74d251fe8405bdb613c177c22a74760322fa3754
Cesar Antonio Morales González <Cesaron@iMac-de-Cesar-2.local>
2017-11-02 15:39:37 -0600

    commit de prueba

--- resources/views/reyapp/main.blade.php
+++ resources/views/reyapp/main.blade.php
@@ -1,7 +1,6 @@
 @extends('layouts.reyapp.landing')
 
 @section('content')
-	Bienvenido Rey App es...
+	Bienvenido Rey App es una app de apps.
 	<a href="/registro" class="button expanded">Empezar</a>
 @endsection
-
