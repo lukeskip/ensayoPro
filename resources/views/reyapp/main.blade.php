2e03d4ad9ecee9e7d6047bdc01bf38f0ee2d5967
Cesar Antonio Morales González <Cesaron@iMac-de-Cesar-2.local>
2017-11-12 17:38:11 -0600

    new style menu & slide, new pics & beneficts design

--- resources/views/reyapp/main.blade.php
+++ resources/views/reyapp/main.blade.php
@@ -7,145 +7,102 @@
 @section('content')
 
 <!-- STARTS: top bar-->
-<div class="title-bar" data-responsive-toggle="responsive-menu" data-hide-for="medium">
-  <button class="menu-icon" type="button" data-toggle="responsive-menu"></button>
-  <div class="title-bar-title">Menu</div>
-</div>
-
-<div class="top-bar" id="responsive-menu">
-  <div class="top-bar-left">
-    <ul class="dropdown menu" data-dropdown-menu>
-      <li class="menu-text"><img src=" http://reyapp.dev:8000/img/logo_rey.png " width="150" alt="logo"></li>
-
-    </ul>
+<nav class="top-bar topbar-responsive">
+  <div class="top-bar-title">
+    <span data-responsive-toggle="topbar-responsive" data-hide-for="medium">
+      <button class="menu-icon" type="button" data-toggle></button>
+    </span>
+    <a class="topbar-responsive-logo" href="#"><img src=" http://reyapp.dev:8000/img/logo_rey.png " width="150" alt="logo"></a>
   </div>
-  <div class="top-bar-right">
-    <ul class="menu">
-      <li><a href="/login/redirect">Iniciar Sesión</a></li>
-      <li><a href="/registro">Registrarme</a></li>
-    </ul>
+  <div id="topbar-responsive" class="topbar-responsive-links">
+    <div class="top-bar-right">
+      <ul class="menu simple vertical medium-horizontal">
+        <li><a href="/login/redirect">Iniciar Sesión</a></li>
+        <li>
+          <button type="button" class="button hollow topbar-responsive-button"><a href="/registro">Registrarme</a></button>
+        </li>
+      </ul>
+    </div>
   </div>
-</div>
+</nav>
 <!-- ENDS: top bar-->
 
 <!--STARTS: slider-->
-<div class="large-12">
-<div class="fullscreen-image-slider">
-  <div class="orbit" role="region" aria-label="FullScreen Pictures" data-orbit>
+<div class="orbit clean-hero-slider" role="region" aria-label="Favorite Space Pictures" data-orbit>
+  <div class="orbit-wrapper">
+    <div class="orbit-controls">
+      <button class="orbit-previous"><i class="fa fa-chevron-left" aria-hidden="true"></i></button>
+      <button class="orbit-next"><i class="fa fa-chevron-right" aria-hidden="true"></i></button>
+    </div>
     <ul class="orbit-container">
-      <button class="orbit-previous">
-        <span class="show-for-sr">Previous Slide</span>
-        <span class="nav fa fa-chevron-left fa-3x"></span>
-      </button>
-      <button class="orbit-next">
-        <span class="show-for-sr">Next Slide</span>
-        <span class="nav fa fa-chevron-right fa-3x"></span>
-      </button>
-      <div class="bottom-content-section" data-magellan data-threshold="0">
-
-
-      <li class="is-active orbit-slide">
-        <img class="orbit-image" src="http:/img/back_bass.jpg" alt="Space">
-        <figcaption class="orbit-caption">
-					<h1>Aquí inicia el músico pro</h1>
-
-        </figcaption>
+      <li class="orbit-slide">
+        <figure class="orbit-figure">
+          <img class="orbit-image" src=" http://reyapp.dev:8000/img/back_bass.jpg" alt="image alt text">
+          <figcaption class="orbit-caption">
+            <h3>Encuentra un espacio adecuado para tu ensayo</h3>
+            <p>Deja de ensayar en la sala o en la vieja cochera, hoy es el día de profesionalizarte.</p>
+          </figcaption>
+        </figure>
       </li>
       <li class="orbit-slide">
-        <img class="orbit-image" src="https://i.imgur.com/JD4Caxa.jpg" alt="Space">
-        <figcaption class="orbit-caption">
-					<h1>Encuenta salas de ensayo cerca de ti</h1>
-        </figcaption>
+        <figure class="orbit-figure">
+          <img class="orbit-image" src="http://reyapp.dev:8000/img/back_drum.jpg" alt="image alt text">
+          <figcaption class="orbit-caption">
+            <h3>Práctica con equipo profesional</h3>
+            <p>¿No tienes para tu amplo soñado? Encuentra la sala con tu equipo ideal.</p>
+          </figcaption>
+        </figure>
       </li>
       <li class="orbit-slide">
-        <img class="orbit-image" src="https://i.imgur.com/rsTQbNV.jpg" alt="Space">
-        <figcaption class="orbit-caption">
-					<h1>toda tu banda conectada</h1>
-        </figcaption>
+        <figure class="orbit-figure">
+          <img class="orbit-image" src="http://reyapp.dev:8000/img/back_guitar.jpg" alt="image alt text">
+          <figcaption class="orbit-caption">
+            <h3>Ensaya en un espacio cómodo</h3>
+            <p>La comodidad es fundamental para que sólo te preocupes por mejorar es a vuelta de coro.</p>
+          </figcaption>
+        </figure>
       </li>
     </ul>
   </div>
-</div>
+  <div class="bottom-content-section" data-magellan data-threshold="0">
+    <a href="#main-content-section"><i class="fa fa-chevron-down fa-3x" aria-hidden="true"></i></a>
+  </div>
+  <nav class="orbit-button">
+    <button class="button expanded"><a href="/registro">Registrarme</a></button>
+  </nav>
 </div>
 {{-- ENDS: slider --}}
 
 <!--STARTS: Benefits -->
+<div id="main-content-section" data-magellan-target="main-content-section">
+  <div class="top-company">
+    <h2>¿Para qué una sala de ensayo?</h2>
+    <p class="text-center">Simple, en una sala de ensayo encontrarás el espacio ideal para ser todo un Pro en la música ya que cuenta con:</p>
+    <div class="row">
+      <div class="row large-up-2 small-up-2">
+        <div class="featured-image-block column">
+          <img src="http://reyapp.dev:8000/img/benefits-time.jpg" />
+          <p class="text-center bullets-company">Tiempo de Calidad</p>
+        </div>
 
-<div class="top-companies">
-  <h2 class="marketing-site-features-headline">¿Para qué una sala de ensayo?</h2>
-  <p class="top-companies-subheadline subheader">Hay miles de beneficios de ensayar en un espacio dedicado a la música:</p>
-  <div class="row">
-    <div class="row large-up-3 small-up-2">
-    <div class="featured-image-block column">
-        <img src="https://unsplash.it/600/440?image=899" />
-        <p class="text-center companies-image-block-title">Tiempo de Calidad</p>
-    </div>
+        <div class="featured-image-block column">
+          <img src="http://reyapp.dev:8000/img/benefits-amp.jpg" />
+          <p class="text-center bullets-company">Equipo Profesional</p>
+        </div>
+        <div class="featured-image-block column">
+          <img src="http://reyapp.dev:8000/img/benefits-confort.jpg" />
+          <p class="text-center bullets-company">Comodidad</p>
+        </div>
 
-    <div class="featured-image-block column">
-        <img src="https://unsplash.it/600/440?image=693" />
-        <p class="text-center companies-image-block-title">Equipo Profesional</p>
-    </div>
-
-    <div class="featured-image-block column">
-        <img src="https://unsplash.it/600/440?image=452" />
-        <p class="text-center companies-image-block-title">Comodidad</p>
-    </div>
-
-    <div class="featured-image-block column">
-        <img src="https://unsplash.it/600/440?image=667" />
-        <p class="text-center companies-image-block-title">Redes Sociales</p>
-    </div>
-
-    <div class="featured-image-block column">
-        <img src="https://unsplash.it/600/440?image=249" />
-        <p class="text-center companies-image-block-title">Calendarización de actividades</p>
-    </div>
-
-    <div class="featured-image-block column">
-        <img src="https://unsplash.it/600/440?image=382" />
-        <p class="text-center companies-image-block-title">Ser todo un Pro</p>
+        <div class="featured-image-block column">
+          <img src="http://reyapp.dev:8000/img/benefits-soundproof.jpg" />
+          <p class="text-center bullets-company">Audio Insonorizado</p>
+        </div>
+      </div>
     </div>
   </div>
-</div>
-<div class="button-float"><a href="/registro" class="button expanded">Registrarme</a></div>
 </div>
 {{-- ENDS: Benefits --}}
-
-
-<!-- STARTS: Features -->
-<div class="marketing-site-features">
-  <h2 class="marketing-site-features-headline">Novedades de nuestra WebApp</h2>
-  <p class="marketing-site-features-subheadline subheader">Ensaya Pro es una app web desarrollada para el músico, por músicos.</p>
-  <div class="row">
-    <div class="small-12 medium-6 large-3 columns">
-      <i class="fa fa-users" aria-hidden="true"></i>
-      <h4 class="marketing-site-features-title">Perfiles</h4>
-      <p class="marketing-site-features-desc">Crea tu perfil y el de tu banda para estar siempre conectado.</p>
-    </div>
-    <div class="small-12 medium-6 large-3 columns">
-      <i class="fa fa-microphone" aria-hidden="true"></i>
-      <h4 class="marketing-site-features-title">Salas de Ensayo Pro</h4>
-      <p class="marketing-site-features-desc">Elige entre las cientos de opciones para que ensayes como Pro cerca de tu localización.</p>
-    </div>
-    <div class="small-12 medium-6 large-3 columns">
-      <i class="fa fa-calendar-check-o" aria-hidden="true"></i>
-      <h4 class="marketing-site-features-title">Control de tus ensayos</h4>
-      <p class="marketing-site-features-desc">Con nuestro calendario no vuelvas a olvidar tus ensayos.</p>
-    </div>
-		<div class="small-12 medium-6 large-3 columns">
-      <i class="fa fa-credit-card-alt" aria-hidden="true"></i>
-      <h4 class="marketing-site-features-title">Pagos protegidos</h4>
-      <p class="marketing-site-features-desc">Paga con tu tarjeta de Crédito o Débito Visa o MasterCard, no temas tus compras están protegidas.</p>
-    </div>
-  </div>
-</div>
-{{-- ENDS: Features --}}
-
-<!--
-<div class="large-12 text-center">
-	<p>Bienvenido al lugar donde te volveras un profesional en la música</p>
-	<a href="/registro" class="button expanded">Registrarme</a> -->
-
 
 @endsection
 
