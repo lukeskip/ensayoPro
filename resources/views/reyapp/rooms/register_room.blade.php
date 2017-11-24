ab3d67c59d1f4b2cf358e3c1a64c94b2a2eab009
Sergio Alberto Garcia Gutierrez <cheko@iMac-de-Sergio.local>
2017-11-24 13:58:35 -0600

    Register room without company fix

--- resources/views/reyapp/rooms/register_room.blade.php
+++ resources/views/reyapp/rooms/register_room.blade.php
@@ -90,235 +90,245 @@
 @endsection
 @section('content')
 
-<div class="form_wrapper">	
-	<form id="form_rooms" class="form-horizontal" method="POST" action="{{ route('companies.store') }}">
-		{{ csrf_field() }}
-		
-		<div class="row">
-			<div class="large-12 columns">
-				@if(Request::has('step'))
-					<div class="step">
-						<div class="description">Paso</div>
-						<div class="number">3</div>
-					</div>
-				@endif
-				
-				<h1>Registra tu sala de ensayo</h1>
-				<h2>Tendrás que registrar una a una las salas con las que cuenta tu marca</h2>
-			</div>
-		</div>
-		
-		<div class="row">
-
-			<input type="hidden" name="company" value="{{$company->id}}">
-
-			<div class="large-12 columns">
+@if($company)
+	<div class="form_wrapper">	
+		<form id="form_rooms" class="form-horizontal" method="POST" action="{{ route('companies.store') }}">
+			{{ csrf_field() }}
 			
-				<label>Nombre</label>
-				<input class="input-group-field required"  type="text" name="name" placeholder="Ej. Sala grande">
-				
-			</div>
-
-		</div>
-
-		<div class="row">
-			<div class="large-12 columns">
-				<h3>Ubicación</h3>
-				<label for="company_address" class="no-background">
-					<input id="company_address" name="company_address" type="checkbox">Misma dirección que tu compañía <i class="fa fa-question-circle hastooltip" aria-hidden="true" title="Activa si la sala está ubicada en la misma dirección que registraste para tu compañía"></i>
-				</label>
-			</div>
-		</div>
-
-		{{-- STARTS: ADDRESS ZONE --}}
-		<div class="new_address">
 			<div class="row">
-				
-				<div class="large-8 columns">	
-					<label>Dirección</label>
-					<input class="input-group-field address get_loc required" type="text" name="address" placeholder="Calle y número" value="">
-				</div>
-				
-				<div class="large-4 columns">
+				<div class="large-12 columns">
+					@if(Request::has('step'))
+						<div class="step">
+							<div class="description">Paso</div>
+							<div class="number">3</div>
+						</div>
+					@endif
 					
-					<label>Delegación o Municipio</label>
-					<input class="input-group-field deputation get_loc required" type="text" name="deputation" placeholder="Delegación o municipio">
-					
-				</div>
-
-			</div>
-
-			<div class="row">
-				
-				<div class="large-4 columns">
-					<label>Colonia</label>
-					<input class="input-group-field colony get_loc required" type="text" name="colony">
-				</div>
-
-				<div class="large-4 columns">
-					<label>Teléfono</label>
-					<input class="input-group-field required" type="text" name="phone">
-				</div>
-
-				<div class="large-4 columns">	
-					<label>Código Postal</label>
-					<input class="input-group-field postal_code get_loc required" type="text" name="postal_code">	
+					<h1>Registra tu sala de ensayo</h1>
+					<h2>Tendrás que registrar una a una las salas con las que cuenta tu marca</h2>
 				</div>
 			</div>
-
+			
 			<div class="row">
-				
-				<div class="large-6 columns">
-					<label>Ciudad</label>
-					<input class="input-group-field city get_loc required" type="text" name="city">
-				</div>
 
-				<div class="large-6 columns">
-					<label>País <i class="fa fa-question-circle hastooltip" title="Por ahora esta plataforma sólo esta disponible en México" aria-hidden="true"></i></label>
-					<select class="input-group-field country get_loc required" name="country" id="">
-						<option value="mexico">México</option>
-					</select>	
-				</div>
+				<input type="hidden" name="company" value="{{$company->id}}">
 
 				<div class="large-12 columns">
-					<div class="clarification text-center">
-						Verifica la ubicación, corrígela arrastrando el pin
-					</div>
-					<div id="map-canvas" ></div>
+				
+					<label>Nombre</label>
+					<input class="input-group-field required"  type="text" name="name" placeholder="Ej. Sala grande">
+					
+				</div>
+
+			</div>
+
+			<div class="row">
+				<div class="large-12 columns">
+					<h3>Ubicación</h3>
+					<label for="company_address" class="no-background">
+						<input id="company_address" name="company_address" type="checkbox">Misma dirección que tu compañía <i class="fa fa-question-circle hastooltip" aria-hidden="true" title="Activa si la sala está ubicada en la misma dirección que registraste para tu compañía"></i>
+					</label>
 				</div>
 			</div>
 
-		</div>
-		{{-- ENDS: ADDRESS ZONE --}}
+			{{-- STARTS: ADDRESS ZONE --}}
+			<div class="new_address">
+				<div class="row">
+					
+					<div class="large-8 columns">	
+						<label>Dirección</label>
+						<input class="input-group-field address get_loc required" type="text" name="address" placeholder="Calle y número" value="">
+					</div>
+					
+					<div class="large-4 columns">
+						
+						<label>Delegación o Municipio</label>
+						<input class="input-group-field deputation get_loc required" type="text" name="deputation" placeholder="Delegación o municipio">
+						
+					</div>
+
+				</div>
+
+				<div class="row">
+					
+					<div class="large-4 columns">
+						<label>Colonia</label>
+						<input class="input-group-field colony get_loc required" type="text" name="colony">
+					</div>
+
+					<div class="large-4 columns">
+						<label>Teléfono</label>
+						<input class="input-group-field required" type="text" name="phone">
+					</div>
+
+					<div class="large-4 columns">	
+						<label>Código Postal</label>
+						<input class="input-group-field postal_code get_loc required" type="text" name="postal_code">	
+					</div>
+				</div>
+
+				<div class="row">
+					
+					<div class="large-6 columns">
+						<label>Ciudad</label>
+						<input class="input-group-field city get_loc required" type="text" name="city">
+					</div>
+
+					<div class="large-6 columns">
+						<label>País <i class="fa fa-question-circle hastooltip" title="Por ahora esta plataforma sólo esta disponible en México" aria-hidden="true"></i></label>
+						<select class="input-group-field country get_loc required" name="country" id="">
+							<option value="mexico">México</option>
+						</select>	
+					</div>
+
+					<div class="large-12 columns">
+						<div class="clarification text-center">
+							Verifica la ubicación, corrígela arrastrando el pin
+						</div>
+						<div id="map-canvas" ></div>
+					</div>
+				</div>
+
+			</div>
+			{{-- ENDS: ADDRESS ZONE --}}
+			
+			<div class="row">
+				<div class="large-12 columns">
+					<h3>Características</h3>
+				</div>
+				<div class="large-6 columns">
+					<label>Descripción <i class="fa fa-question-circle hastooltip" title="Escribe una descripción breve, ej: Sala amplia cómoda para bandas de hasta 6 miembros" aria-hidden="true"></i></label>
+					<textarea class="input-group-field required" type="text" name="description"></textarea>
+				</div>
+
+				<div class="large-6 columns">		
+					<label>Equipamiento <i class="fa fa-question-circle hastooltip" title="Escribe un item por línea, ej. Amplificador Marshall a Bulbos mod..." aria-hidden="true"></i></label>
+					<textarea class="input-group-field required" type="text" name="equipment" placeholder="Escribe un item por línea"></textarea>
+				</div>
+
+			</div>
+
+			<div class="row">
+			   
+				<div class="large-6 columns">
+					<label>Precio <i class="fa fa-question-circle hastooltip" title="Escribe el precio de renta de esta sala por hora" aria-hidden="true"></i></label>
+					<input class="input-group-field required" type="text" name="price">
+				</div>
+
+				<div class="large-6 columns">
+					<label>
+						Color 
+						<i class="fa fa-question-circle hastooltip" title="Con este color identificarás a esta sala en el calendario" aria-hidden="true"></i>
+					</label>
+					<input class="input-group-field colorpicker required" type="text" name="color">
+				</div>
+
+			</div>
+			<div class="row">
+				<div class="large-12 columns">
+					<h3>Horario de servicio</h3>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="large-4 columns">
+					<label for="">Días de Servicio</label>
+					<select data-placeholder="Selecciona..." multiple name="days[]" class="chosen-select required">
+					  <option value="-1">Todos los días</option>
+					  <option value="1">Lunes</option>
+					  <option value="2">Martes</option>
+					  <option value="3">Miércoles</option>
+					  <option value="4">Jueves</option>
+					  <option value="5">Viernes</option>
+					  <option value="6">Sábado</option>
+					  <option value="0">Domingo</option>
+
+					</select>
+					<label class="chosen-error error"></label>
+				</div>
+
+
+				<div class="large-4 columns">
+					<label>Abre:</label>
+					<select class="input-group-field required" name="schedule_start" id="">
+						<option value="">Selecciona...</option>
+						<option value="6">6:00</option>
+						<option value="7">7:00</option>
+						<option value="8">8:00</option>
+						<option value="9">9:00</option>
+						<option value="10">10:00</option>
+						<option value="11">11:00</option>
+						<option value="12">12:00</option>
+						<option value="13">13:00</option>
+						<option value="14">14:00</option>
+						<option value="15">15:00</option>
+						<option value="16">16:00</option>
+						<option value="17">17:00</option>
+						<option value="18">18:00</option>
+						<option value="19">19:00</option>
+						<option value="20">20:00</option>
+						<option value="21">21:00</option>
+						<option value="22">22:00</option>
+						<option value="23">23:00</option>
+						<option value="24">24:00</option>
+					</select>
+					
+				</div>
+
+				<div class="large-4 columns">
+					<label>Cierra</label>
+					<select class="input-group-field required" name="schedule_end" id="">
+						<option value="">Selecciona...</option>
+						<option value="6">6:00</option>
+						<option value="7">7:00</option>
+						<option value="8">8:00</option>
+						<option value="9">9:00</option>
+						<option value="10">10:00</option>
+						<option value="11">11:00</option>
+						<option value="12">12:00</option>
+						<option value="13">13:00</option>
+						<option value="14">14:00</option>
+						<option value="15">15:00</option>
+						<option value="16">16:00</option>
+						<option value="17">17:00</option>
+						<option value="18">18:00</option>
+						<option value="19">19:00</option>
+						<option value="20">20:00</option>
+						<option value="21">21:00</option>
+						<option value="22">22:00</option>
+						<option value="23">23:00</option>
+						<option value="24">24:00</option>
+					</select>
+				</div>
+			</div>
 		
-		<div class="row">
-			<div class="large-12 columns">
-				<h3>Características</h3>
-			</div>
-			<div class="large-6 columns">
-				<label>Descripción <i class="fa fa-question-circle hastooltip" title="Escribe una descripción breve, ej: Sala amplia cómoda para bandas de hasta 6 miembros" aria-hidden="true"></i></label>
-				<textarea class="input-group-field required" type="text" name="description"></textarea>
 			</div>
 
-			<div class="large-6 columns">		
-				<label>Equipamiento <i class="fa fa-question-circle hastooltip" title="Escribe un item por línea, ej. Amplificador Marshall a Bulbos mod..." aria-hidden="true"></i></label>
-				<textarea class="input-group-field required" type="text" name="equipment" placeholder="Escribe un item por línea"></textarea>
+			<div class="row">
+				<div class="large-12 columns">
+					<div id="uploader"></div>
+					<br><br>
+				</div>
 			</div>
 
-		</div>
-
-		<div class="row">
-		   
-			<div class="large-6 columns">
-				<label>Precio <i class="fa fa-question-circle hastooltip" title="Escribe el precio de renta de esta sala por hora" aria-hidden="true"></i></label>
-				<input class="input-group-field required" type="text" name="price">
+			<div class="row ">
+				<div class="large-12 columns">
+					<button type="submit" class="button expanded green register_room submit">
+						Registrar
+					</button>
+				</div>
 			</div>
-
-			<div class="large-6 columns">
-				<label>
-					Color 
-					<i class="fa fa-question-circle hastooltip" title="Con este color identificarás a esta sala en el calendario" aria-hidden="true"></i>
-				</label>
-				<input class="input-group-field colorpicker required" type="text" name="color">
-			</div>
-
-		</div>
-		<div class="row">
-			<div class="large-12 columns">
-				<h3>Horario de servicio</h3>
-			</div>
-		</div>
-
-		<div class="row">
-			<div class="large-4 columns">
-				<label for="">Días de Servicio</label>
-				<select data-placeholder="Selecciona..." multiple name="days[]" class="chosen-select required">
-				  <option value="-1">Todos los días</option>
-				  <option value="1">Lunes</option>
-				  <option value="2">Martes</option>
-				  <option value="3">Miércoles</option>
-				  <option value="4">Jueves</option>
-				  <option value="5">Viernes</option>
-				  <option value="6">Sábado</option>
-				  <option value="0">Domingo</option>
-
-				</select>
-				<label class="chosen-error error"></label>
-			</div>
-
-
-			<div class="large-4 columns">
-				<label>Abre:</label>
-				<select class="input-group-field required" name="schedule_start" id="">
-					<option value="">Selecciona...</option>
-					<option value="6">6:00</option>
-					<option value="7">7:00</option>
-					<option value="8">8:00</option>
-					<option value="9">9:00</option>
-					<option value="10">10:00</option>
-					<option value="11">11:00</option>
-					<option value="12">12:00</option>
-					<option value="13">13:00</option>
-					<option value="14">14:00</option>
-					<option value="15">15:00</option>
-					<option value="16">16:00</option>
-					<option value="17">17:00</option>
-					<option value="18">18:00</option>
-					<option value="19">19:00</option>
-					<option value="20">20:00</option>
-					<option value="21">21:00</option>
-					<option value="22">22:00</option>
-					<option value="23">23:00</option>
-					<option value="24">24:00</option>
-				</select>
-				
-			</div>
-
-			<div class="large-4 columns">
-				<label>Cierra</label>
-				<select class="input-group-field required" name="schedule_end" id="">
-					<option value="">Selecciona...</option>
-					<option value="6">6:00</option>
-					<option value="7">7:00</option>
-					<option value="8">8:00</option>
-					<option value="9">9:00</option>
-					<option value="10">10:00</option>
-					<option value="11">11:00</option>
-					<option value="12">12:00</option>
-					<option value="13">13:00</option>
-					<option value="14">14:00</option>
-					<option value="15">15:00</option>
-					<option value="16">16:00</option>
-					<option value="17">17:00</option>
-					<option value="18">18:00</option>
-					<option value="19">19:00</option>
-					<option value="20">20:00</option>
-					<option value="21">21:00</option>
-					<option value="22">22:00</option>
-					<option value="23">23:00</option>
-					<option value="24">24:00</option>
-				</select>
-			</div>
-		</div>
-	
-		</div>
-
-		<div class="row">
-			<div class="large-12 columns">
-				<div id="uploader"></div>
-				<br><br>
-			</div>
-		</div>
-
-		<div class="row ">
-			<div class="large-12 columns">
-				<button type="submit" class="button expanded green register_room submit">
-					Registrar
-				</button>
-			</div>
-		</div>
-	</form>
+		</form>
+	</div>
+@else
+<div class="row">
+	<div class="large-12 columns text-center">
+		<h1>Primero registra tu compañía</h1>
+		<a class="button green" href="/company/registro">Registrar Compañía</a>
+	</div>
 </div>
+	
+@endif	
 @endsection
 @section('scripts')
 	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCMMiHmdWoL0K5FQPWL_cXBbK0IV-t7l3w"></script>
