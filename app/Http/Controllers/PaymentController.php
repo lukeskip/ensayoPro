71736ecfb35842f78a3c59e212639fa4b944dab2
Sergio Alberto Garcia Gutierrez <cheko@iMac-de-Sergio.local>
2017-11-29 18:06:17 -0600

    Webhook test 19

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -470,41 +470,45 @@ class PaymentController extends Controller
 
 				$payment = Payment::where('order_id',$order_id)->first();
 
-				$payment->status = $status;
-				$room = $payment->reservations->first()->rooms;
-				$email = $payment->reservations->first()->users->email;
-				foreach ($payment->reservations as $reservation) {
+				if($payment->status == 'pending_payment'){
+
+				
+					$payment->status = $status;
+					$room = $payment->reservations->first()->rooms;
+					$email = $payment->reservations->first()->users->email;
+					foreach ($payment->reservations as $reservation) {
 
 
-					$reservation->status = 'confirmed';
-					$reservation->save();
+						$reservation->status = 'confirmed';
+						$reservation->save();
 
-					// formateamos el inicio y fin del ensayo para envio de correo
-    				$mail_starts  = new Date($reservation->starts);
-    				$mail_ends    = new Date($reservation->starts);
-    				$events[] = array(
-    					'mail_time'=> $mail_starts->format('H:i').' a '.$mail_ends->format('H:i'),
-    					'mail_date'=> $mail_starts->format('l j F Y '),
-    				);
-				}
+						// formateamos el inicio y fin del ensayo para envio de correo
+	    				$mail_starts  = new Date($reservation->starts);
+	    				$mail_ends    = new Date($reservation->ends);
+	    				$events[] = array(
+	    					'mail_time'=> $mail_starts->format('H:i').' a '.$mail_ends->format('H:i'),
+	    					'mail_date'=> $mail_starts->format('l j F Y '),
+	    				);
+					}
 
-				$room_name  	= $room->name;
-				$latitude		= $room->latitude;
-				$longitude		= $room->latitude;
-				$instructions 	= $room->instructions;
-				$company 		= $room->companies->name;
-				$address        = $room->address.', '.$room->colony.', '.$room->deputation.', '.$room->city;
+					$room_name  	= $room->name;
+					$latitude		= $room->latitude;
+					$longitude		= $room->latitude;
+					$instructions 	= $room->instructions;
+					$company 		= $room->companies->name;
+					$address        = $room->address.', '.$room->colony.', '.$room->deputation.', '.$room->city;
 
 
-				$payment->save();
+					$payment->save();
 
-				// Enviamos correo de confirmación
-				Mail::send('reyapp.mails.confirmation', ['room_name'=>$room_name,'events'=>$events,'latitude'=>$latitude,'longitude'=>$longitude,'address'=>$address,'company'=>$company,'instructions'=>$instructions], function ($message)use($email,$company){
+					// Enviamos correo de confirmación
+					Mail::send('reyapp.mails.confirmation', ['room_name'=>$room_name,'events'=>$events,'latitude'=>$latitude,'longitude'=>$longitude,'address'=>$address,'company'=>$company,'instructions'=>$instructions], function ($message)use($email,$company){
 
-                $message->from('no_replay@ensayopro.com.mx', 'EnsayoPro')->subject('Tienes una reservación en '.$company);
-                $message->to($email);
+	                $message->from('no_replay@ensayopro.com.mx', 'EnsayoPro')->subject('Tienes una reservación en '.$company);
+	                $message->to($email);
 
-                });
+	                });
+	            }
 			}		
 			
 		}
