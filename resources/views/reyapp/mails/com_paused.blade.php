6ebdd948fe5d433fcd4b82ff41c353e6e7bd800b
Sergio Alberto Garcia Gutierrez <cheko@iMac-de-Sergio.local>
2017-12-11 20:03:46 -0600

    cron mail fix

--- resources/views/reyapp/mails/com_paused.blade.php
+++ resources/views/reyapp/mails/com_paused.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.reyapp.mail')
 @section('image')
-<img src="{{asset('img/invitation.jpg')}}">
+<br><br>
+<h1><img src="{{asset('img/logo_ensayo.png')}}" alt="EnsayoPro"></h1>
 @endsection
 
 @section('content')
