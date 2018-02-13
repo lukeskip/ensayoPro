84a100fd2df9250d45a934dfde3e4f3de0f688b4
Sergio Alberto Garcia Gutierrez <cheko@iMac-de-iMac.local>
2018-02-13 10:00:15 -0600

    mailchimp fix

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -99,7 +99,7 @@ class RegisterController extends Controller
             'merge_fields'  => array(
                 'FNAME' => $fname,
                 'LNAME' => $lname,
-                'TYPE' => $user->roles->first(),
+                'TYPE' => $user->roles()->first()->name,
                 )
         );
         $json_data = json_encode($data);
