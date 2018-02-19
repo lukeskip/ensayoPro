e78235b065666e72cabac881a07ebc72697ca8b7
Sergio Alberto Garcia Gutierrez <cheko@iMac-de-iMac.local>
2018-02-19 16:27:26 -0600

    kernel_test

--- resources/views/reyapp/mails/test_kernel.blade.php
+++ resources/views/reyapp/mails/test_kernel.blade.php
@@ -5,21 +5,7 @@
 @endsection
 
 @section('content')
-  	<h1>Tu cuenta ha sido pausada</h1>
-  	<p>Debido a que notamos que no te has conectado a EnsayoPro en {{$max_hours}} horas, hemos pausado tu cuenta para evitar malos entendidos con los reservantes en caso de que por alguna razón no estes utilizando la plataforma para generar las reservaciones.</p>
-	<p>
-
-	<p>Para reactivar tu cuenta solo tienes que logearte</p>
-
-	<br><br>
-
-	<a href="{{url('/')}}/login" style="background: #2FAB31;color:white;padding: 15px; text-align: center; border-radius: 10px; text-decoration: none;font-size: 20px;">
-		Login
-	</a>
-
-	</p>
-	
-	<br><br>
+  	{{$reservations}}
 
 
 @endsection
