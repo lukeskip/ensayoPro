cb1c499c98aadcc7960b172385587dd3d759a032
Sergio Alberto Garcia Gutierrez <cheko@iMac-de-iMac.local>
2018-03-02 01:25:35 -0600

    facebook test

--- app/Http/Controllers/SocialAuthFacebookController.php
+++ app/Http/Controllers/SocialAuthFacebookController.php
@@ -32,7 +32,7 @@ class SocialAuthFacebookController extends Controller
     {
         $user = $service->createOrGetUser(Socialite::driver('facebook')->stateless()->user());
         auth()->login($user);
-        return redirect(session('goto'));
+        return redirect()->to(session('goto'));
         
     }
 }
